2a55e7ae2e31addafa3bf9524b7b39c23c78300a
Ahmed Adejumat <medonfreed@gmail.com>
2017-01-10 03:45:21 +0400

    Email config

---
+++ application/config/email.php
@@ -0,0 +1,11 @@
+<?php
+
+// Email settings
+
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'smtp.gmail.com';
+$config['smtp_user'] = '';
+$config['smtp_pass'] = '';
+$config['smtp_crypto'] = 'tls';
+$config['mailtype'] = 'html';
+$config['smtp_port'] = 587;
